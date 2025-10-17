7df961341ebaf6681498755cf196b6818c1e7eda
Alpha1714 <alexpalomino2511@gmail.com>
2025-10-16 21:53:52 -0500

    Aumento la funcion recuperar entero y flotante

--- Utilitarios/saludo.js
+++ Utilitarios/saludo.js
@@ -1,7 +1,10 @@
 saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarEntero("txtEdad");
+    let altura=RecuperarFloat("txtEstatura");
     console.log("Hola "+nombre+" "+apellido);
+    console.log("Tienes "+edad+" años y mides "+altura+" metros.");
 }
 recuperarTexto=function(idComponente){
     let componente;
@@ -9,4 +12,15 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+recuperarEntero=function(idComponente){
+    let valorCaja=document.getElementById(idComponente);
+    let valorEntero=parseInt(valorCaja.value);
+    return valorEntero;
+}
+
+RecuperarFloat=function(idComponente){
+    let valorCaja=document.getElementById(idComponente);
+    let valorFlotante=parseFloat(valorCaja.value);
+    return valorFlotante;
 }

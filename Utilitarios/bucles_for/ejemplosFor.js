b34974efa2a9a1c1449cfb7d1da58a3b55f3262a
Alpha1714 <alexpalomino2511@gmail.com>
2025-10-23 19:24:33 -0500

    Se formateo el codigo para qeu se mierar mas estetico y no exista problemas al usarlo

--- Utilitarios/bucles_for/ejemplosFor.js
+++ Utilitarios/bucles_for/ejemplosFor.js
@@ -1,45 +1,45 @@
-mostrarNumeros = function(){
+mostrarNumeros = function () {
     console.log("Antes del for");
-for (let i=0;i<4;i++){
-    console.log(i);
-}
+    for (let i = 0; i < 4; i++) {
+        console.log(i);
+    }
     console.log("Despues del for");
 }
 
-mostrarNumeros2 = function(){
+mostrarNumeros2 = function () {
     console.log("Antes del for");
-for (let indice=1;indice<=5;indice++){
-    console.log(indice);
-}
+    for (let indice = 1; indice <= 5; indice++) {
+        console.log(indice);
+    }
     console.log("Despues del for");
 }
 
-mostrarPares = function(){
+mostrarPares = function () {
     console.log("Antes del for");
-for(let x=2; x<=10 ; x+=2){
-    console.log(x)
-}
+    for (let x = 2; x <= 10; x += 2) {
+        console.log(x)
+    }
     console.log("Despues del for");
 }
 
-mostrarInverso = function(){
+mostrarInverso = function () {
     console.log("Antes del for");
-for(let i=10 ; i>=0 ; i--){
-    console.log(i)
-}
+    for (let i = 10; i >= 0; i--) {
+        console.log(i)
+    }
     console.log("Despues del for");
 }
 
-mostrarNasa = function(){
-    for(let i=0 ; i<=100 ; i+=10){
+mostrarNasa = function () {
+    for (let i = 0; i <= 100; i += 10) {
         console.log("Hackeanado en " + i + "%")
     }
-       console.log(" Ha sido hackeada") 
+    console.log(" Ha sido hackeada")
 }
 
-mostrarImpares = function(){
+mostrarImpares = function () {
     console.log("Antes del for");
-    for(i=1 ; i<=21 ; i+=2){
+    for (i = 1; i <= 21; i += 2) {
         console.log(i)
     }
     console.log("Despues del for");

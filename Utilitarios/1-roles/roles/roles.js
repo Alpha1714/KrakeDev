8060952f07d45899d70af1786cec58dcae32aa9c
Alpha1714 <alexpalomino2511@gmail.com>
2025-10-24 23:00:12 -0500

    Se organizo y ternmino las diferetes faces  del proyecto

--- Utilitarios/1-roles/roles/roles.js
+++ Utilitarios/1-roles/roles/roles.js
@@ -1,9 +1,9 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
 ]
 
-mostrarOpcionEmpleado = function(){
+mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
@@ -11,7 +11,7 @@ mostrarOpcionEmpleado = function(){
     deshabilitarAlGuardar();
 }
 
-mostrarOpcionRol = function(){
+mostrarOpcionRol = function () {
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
     ocultarComponente("divResumen");
@@ -21,10 +21,32 @@ mostrarOpcionRol = function(){
 
 }
 
-mostrarOpcionResumen = function(){
+mostrarOpcionResumen = function () {
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
     mostrarRoles();
     mostrarTotales();
+}
+
+mostrarEmpleados = function () {
+
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let encabezado = "<tr><th>Cedula</th><th>Nombre</th><th>Apellido</th><th>Sueldo</th></tr>"
+    let tabla = `<table class="tabla-empleados">` + encabezado;
+    let elemento;
+
+    for (let i = 0; i < empleados.length; i++) {
+        elemento = empleados[i];
+        tabla += "<tr>"
+        tabla += "<td>" + elemento.cedula + "</td>"
+        tabla += "<td>" + elemento.nombre + "</td>"
+        tabla += "<td>" + elemento.apellido + "</td>"
+        tabla += "<td>" + elemento.sueldo + "</td>"
+        tabla += "</tr>"
+    }
+
+    tabla += "</table>"
+    cmpTabla.innerHTML = tabla;
+
 }
